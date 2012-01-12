2414da5e1129f2927d8351d059d14ec20058fd3e
João Soares <jsoaresgeral@gmail.com>
2012-01-12 22:25:48 +0000

    first migration examples created

---
+++ db/migrate/20120112192842_do_nothing_yet.rb
@@ -0,0 +1,9 @@
+class DoNothingYet < ActiveRecord::Migration
+  def up
+  	# instructions for changing the database to a new state
+  end
+
+  def down
+  	# instructions for changing the database back to a previous state
+  end
+end
