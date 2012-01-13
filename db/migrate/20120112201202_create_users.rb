6a70c3a8e376ac0eb8276cf807e5a34ae98553eb
João Soares <jsoaresgeral@gmail.com>
2012-01-13 02:08:16 +0000

    changed create_users migration /change/ method to /self.up/

--- db/migrate/20120112201202_create_users.rb
+++ db/migrate/20120112201202_create_users.rb
@@ -1,5 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :users do |t|
       t.string "first_name", :limit => 25
       t.string "last_name", :limit => 50
@@ -7,5 +7,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string "password", :limit => 40
       t.timestamps
     end
+  end
+
+  def self.down
+  	drop_table :users
   end
 end
