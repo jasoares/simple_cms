2f4bc867b3cdb1229156431bee23fbb3ebc7bc10
João Soares <jsoaresgeral@gmail.com>
2012-01-25 21:31:52 +0000

    Added a database external :password attribute to the model for use with forms. Added self.make_salt method to make salt based on username, Time.now and some string. Added self.hash_with_salt method to produce the hashed password based on the password and the salt. Added self.authenticate method to authenticate a user based on a pure text password and username, this method makes use of a auxiliary method called self.password_match?. Added the private methods create_hashed_password and clear_password called through a callback from before_save and after_save based on the :password attribute. In a nutshel just added the user authentication mechanism necessary to login users securely through sha1 hashed random+static salted passwords.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,4 @@
+require 'digest/sha1'
 class AdminUser < ActiveRecord::Base
 	# To configure a different table name
 	# set_table_name("admin_users")
@@ -5,6 +6,8 @@ class AdminUser < ActiveRecord::Base
 	has_many :section_edits
 	has_many :sections, :through => :section_edits
 
+	attr_accessor :password
+	
 	EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 	
 	# standard validation methods
@@ -27,5 +30,53 @@ class AdminUser < ActiveRecord::Base
 	validates :email, :presence => true, :length => {:maximum => 100}, 
 		:format => EMAIL_REGEX, :confirmation => true
 	
+	# only on create, so other attributes of this user can be changed
+	validates_length_of :password, :within => 8..25, :on => :create
+
+	before_save :create_hashed_password
+	after_save :clear_password
+
 	scope :named, lambda {|first, last| where(:first_name => first, :last_name => last)}
+
+	attr_protected :hashed_password, :salt
+
+	def self.authenticate(username="", password="")
+		user = AdminUser.find_by_username(username)
+		if user && user.password_match?(password)
+			return user
+		else
+			return false
+		end
+	end
+
+	# The same password string with the same hash method and salt
+	# should always generate the same hashed_password.
+	def password_match?(password="")
+		hashed_password == AdminUser.hash_with_salt(password, salt)
+	end
+
+	def self.make_salt(username="")
+		Digest::SHA1.hexdigest("Use #{username} with #{Time.now} to make salt")
+	end
+
+	def self.hash_with_salt(password="", salt="")
+		Digest::SHA1.hexdigest("Put #{salt} on the #{password}")
+	end
+
+	private
+
+	def create_hashed_password
+		# Whenever :password has a value hashing is needed
+		unless password.blank?
+			# always use "self" when assigning values
+			self.salt = AdminUser.make_salt(username) if salt.blank?
+			self.hashed_password = AdminUser.hash_with_salt(password, salt)
+		end
+	end
+
+	def clear_password
+		# for security and b/c hashing is not needed
+		self.password = nil
+	end
+
 end
