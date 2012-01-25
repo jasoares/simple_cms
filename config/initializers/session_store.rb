23ffed93d330fd59b91630be2ce532c950412813
João Soares <jsoaresgeral@gmail.com>
2012-01-25 23:19:40 +0000

    corrected syntax error on rails generated configuration file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SimpleCms::Application.config.session_store :cookie_store, key: '_simple_cms_session'
+SimpleCms::Application.config.session_store :cookie_store, :key => '_simple_cms_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
