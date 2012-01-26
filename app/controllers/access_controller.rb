378ecd1f0c0e8b075421081f0cbb1c164fd54b44
João Soares <jsoaresgeral@gmail.com>
2012-01-26 00:34:38 +0000

    moved confirm_logged_in method to ApplicationController and made it protected to be accessible from all other controllers and their before_filter callbacks

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -37,16 +37,4 @@ class AccessController < ApplicationController
     redirect_to(:action => 'login')
   end
 
-  private
-
-  def confirm_logged_in
-    unless session[:user_id]
-      flash[:notice] = "Please log in."
-      redirect_to(:action => 'login')
-      return false # halts the before_filter
-    else
-      return true
-    end
-  end
-
 end
